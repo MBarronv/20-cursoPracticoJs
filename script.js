87161d384288864353a8cfa9b6736d64ec2a8229
MBarronv <ekimran@gmail.com>
2023-03-30 14:04:59 -0600

    clase 13 1ra  forma de unir funcion js con el html

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("Limpiamos el script.js");
+const h1 = document.querySelector('h1');
+const input1 = document.querySelector('#cal1');
+const input2 = document.querySelector('#cal2');
+const btn = document.querySelector('#calcular');
+const result = document.querySelector('#result');
+
+
+function btnClick(){
+    console.log('escuchando click');
+    //Se usa la funcion de number para convertir el tipo string de un input a numero
+    console.log(`${Number(input1.value) + Number(input2.value)}`);
+    result.innerHTML = Number(input1.value) + Number(input2.value);
+}
