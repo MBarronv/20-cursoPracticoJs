917ba21f90194b47104683bd53fa2ac0e6a76952
MBarronv <ekimran@gmail.com>
2023-04-18 10:38:50 -0600

    Clase 14 addEventListener con formulario, submit event.preventDefault

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const h1 = document.querySelector('h1');
+const form = document.querySelector('#form');
 const input1 = document.querySelector('#cal1');
 const input2 = document.querySelector('#cal2');
 const btn = document.querySelector('#calcular');
@@ -6,10 +7,13 @@ const result = document.querySelector('#result');
 
 // se crea un evento de escucha con el atributo click, y el comando a ejecutar
 // cabe mencionar que el comando siendo un metodo no se llama como tal 
-btn.addEventListener( 'click', btnClick );
+form.addEventListener( 'submit', btnClick );
 
-function btnClick(){
+function btnClick(event){
+    console.log({event})
+    event.preventDefault();
     console.log('escuchando click');
+    
     //Se usa la funcion de number para convertir el tipo string de un input a numero
     console.log(`${Number(input1.value) + Number(input2.value)}`);
     result.innerHTML = Number(input1.value) + Number(input2.value);
